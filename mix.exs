2cb65d56d69bc4e3a96dd1338a04fd12243ec356
Kilian Cirera Sant <kilian@paradem.co>
2016-02-01 00:18:30 +0100

    Up to version 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixTimex.Mixfile do
 
   def project do
     [app: :phoenix_timex,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      package: package,
      description: "Phoenix and Timex integration",
