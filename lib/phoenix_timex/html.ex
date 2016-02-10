84ade9a06ea683eb284aaff6d182ac42cbc9efaf
Kilian Cirera Sant <kilian@paradem.co>
2016-02-10 11:32:52 +0100

    Guess when to just show the date without the time

--- lib/phoenix_timex/html.ex
+++ lib/phoenix_timex/html.ex
@@ -1,17 +1,14 @@
 if Code.ensure_loaded?(Phoenix.HTML) do
-  defimpl Phoenix.HTML.Safe, for: [Timex.DateTime, Timex.Ecto.DateTime] do
+  use Timex
+
+  defimpl Phoenix.HTML.Safe, for: [DateTime] do
     def to_iodata(t) do
-      Timex.DateFormat.format!(t, "%F %T", :strftime)
-    end
-  end
-  defimpl Phoenix.HTML.Safe, for: [Timex.Date, Timex.Ecto.Date] do
-    def to_iodata(t) do
-      Timex.DateFormat.format!(t, "%F", :strftime)
-    end
-  end
-  defimpl Phoenix.HTML.Safe, for: [Timex.Time, Timex.Ecto.Time] do
-    def to_iodata(t) do
-      Timex.DateFormat.format!(t, "%T", :strftime)
+      case t |> Date.universal do
+        %{hour: 0, minute: 0, second: 0, ms: 0} ->
+          DateFormat.format!(t, "%F", :strftime)
+        _ ->
+          DateFormat.format!(t, "%F %T", :strftime)
+      end
     end
   end
 end
