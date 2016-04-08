e7815bbc9c20bc68f55367d9560253d0d7cd8c57
Kilian Cirera Sant <kilian@paradem.co>
2016-04-08 14:06:32 +0200

    Add support for date, update formatting code to Timex 2.x, add tests

--- test/phoenix_timex_test.exs
+++ test/phoenix_timex_test.exs
@@ -1,8 +1,16 @@
 defmodule PhoenixTimexTest do
   use ExUnit.Case
+  use Timex
+
   doctest PhoenixTimex
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "DateTime" do
+    datetime = DateTime.from({{2016,2,29}, {12, 30, 30, 120}}, "Europe/Brussels")
+    assert Phoenix.HTML.Safe.to_iodata(datetime) == "2016-02-29 12:30:30"
+  end
+
+  test "Date" do
+    date = %Date{year: 2016, month: 2, day: 29}
+    assert Phoenix.HTML.Safe.to_iodata(date) == "2016-02-29"
   end
 end
