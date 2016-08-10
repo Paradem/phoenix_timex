363ebd1bc39097c358b172e7195c0726246094c9
Shane Thomas <shane@devshane.com>
2016-08-10 11:27:09 -0400

    bump phoenix_html version to 2.6

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule PhoenixTimex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:phoenix_html, "~> 2.5"},
+    [{:phoenix_html, "~> 2.6"},
      {:timex, "~> 3.0"},
     ]
   end
